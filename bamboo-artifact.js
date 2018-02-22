ef6df626e43d19dbf1611361e6b4e3d79f8e0f83
kdogan <kdogan@thoughtworks.com>
2018-02-22 13:37:41 +0300

    editorconfig file and re-format

--- bamboo-artifact.js
+++ bamboo-artifact.js
@@ -2,11 +2,11 @@ document.querySelector('body').addEventListener('click', function(e){
 	var artifactURL = e.target.href;
 	if( artifactURL && artifactURL.indexOf('app_versions.json') !== -1){
 		e.preventDefault();
-        !e.target.hasAttribute('data-tooltip') && fetch(artifactURL,{credentials: 'include'})
-			.then(resp => resp.json())
+    !e.target.hasAttribute('data-tooltip') && fetch(artifactURL,{credentials: 'include'})
+      .then(resp => resp.json())
 			.then(function(data){
 				var artifactList = [];
-            	for(var i in data){ artifactList.push( (i.padEnd(15,' ') + ' : ' + data[i].padEnd(15,' ') ) ) }
+        for(var i in data){ artifactList.push( ( i.padEnd(15,' ') + ' : ' + data[i].padEnd(15,' ') ) ) }
 				e.target.setAttribute('data-tooltip',artifactList.join('\n'));
 			})
 	}
