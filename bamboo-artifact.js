e01253adcd11a03deefdd7fbbc72ac328c05e364
Ceyhun Can ULKER <ceyhun.ulker@sony.com>
2018-02-23 10:37:51 +0300

    Listen to page navigation events and add tooltip if anchor is available
    Fixes the case when you land on a different tab, then navigate to
    'Artifacts' tab, since the tooltip addition has already been tried and
    failed.

--- bamboo-artifact.js
+++ bamboo-artifact.js
@@ -1,9 +1,17 @@
-var target = document.querySelector('a[href$=app_versions\\.json]');
-var artifactURL = target.href;
-!target.hasAttribute('data-tooltip') && fetch(artifactURL,{credentials: 'include'})
-  .then(resp => resp.json())
-  .then(function(data){
-    var artifactList = [];
-    for(var i in data){ artifactList.push( ( i.padEnd(15,' ') + ' : ' + data[i].padEnd(15,' ') ) ) }
-    target.setAttribute('data-tooltip',artifactList.join('\n'));
-  });
+function refreshTooltip() {
+  var target = document.querySelector('a[href$=app_versions\\.json]');
+  var artifactURL = target && target.href;
+  target && !target.hasAttribute('data-tooltip') && fetch(artifactURL,{credentials: 'include'})
+    .then(resp => resp.json())
+    .then(function(data){
+      var artifactList = [];
+      for(var i in data){ artifactList.push( ( i.padEnd(15,' ') + ' : ' + data[i].padEnd(15,' ') ) ) }
+      target.setAttribute('data-tooltip',artifactList.join('\n'));
+    });
+}
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  setTimeout(refreshTooltip, 500);
+});
+
+refreshTooltip();
